4199adff47e66ac6814a80d83d9c78eca2c64e3e
shahroz.khan <shehroz.altaf92@gmail.com>
2020-02-03 18:05:37 +0500

    pages

--- application/views/settings/pages.php
+++ application/views/settings/pages.php
@@ -1,41 +1,37 @@
 <!-- BEGIN: Content-->
 <div class="app-content content">
     <div class="content-wrapper">
-
         <div class="content-wrapper-before"></div>
         <div class="content-header row">
             <div class="content-header-left col-md-8 col-12 mb-2 breadcrumb-new">
-                <h3 class="content-header-title mb-0 d-inline-block">Group</h3>
+                <h3 class="content-header-title mb-0 d-inline-block">Page</h3>
                 <div class="breadcrumbs-top d-inline-block">
                     <div class="breadcrumb-wrapper mr-1">
                         <ol class="breadcrumb">
                             <li class="breadcrumb-item"><a href="<?php echo base_url() ?>">Home</a>
                             </li>
-                            <li class="breadcrumb-item active">Groups
+                            <li class="breadcrumb-item active">Pages
                             </li>
                         </ol>
                     </div>
                 </div>
             </div>
             <div class="content-header-right col-md-4 col-12 d-block d-md-none"><a
-                    class="btn btn-warning btn-min-width float-md-right box-shadow-4 mr-1 mb-1"
-                    href="<?php echo base_url() ?>"><i class="ft-mail"></i> Groups</a>
-
+                        class="btn btn-warning btn-min-width float-md-right box-shadow-4 mr-1 mb-1"
+                        href="<?php echo base_url() ?>"><i class="ft-mail"></i> Pages</a>
             </div>
         </div>
-
         <div class="content-body">
             <section id="ordering">
                 <div class="row">
                     <div class="col-12">
                         <div class="card">
                             <div class="card-header">
-                                <h4 class="card-title">Groups
+                                <h4 class="card-title">Pages
                                     <button type="button" class="btn bg-gradient-x-blue-cyan white addbtn">
                                         <i class="ft-plus"></i> Add New
                                     </button>
                                 </h4>
-
                                 <a class="heading-elements-toggle"><i class="la la-ellipsis-v font-medium-3"></i></a>
                                 <div class="heading-elements">
                                     <ul class="list-inline mb-0">
@@ -54,20 +50,22 @@
                                             <thead>
                                             <tr>
                                                 <th>SNo</th>
-                                                <th>Group</th>
+                                                <th>Page</th>
+                                                <th>URL</th>
                                                 <th>Action</th>
                                             </tr>
                                             </thead>
                                             <tbody>
                                             <?php
                                             $SNo = 0;
-                                            if (isset($groups) && $groups != '') {
-                                                foreach ($groups as $group) {
+                                            if (isset($pages) && $pages != '') {
+                                                foreach ($pages as $page) {
                                                     $SNo++; ?>
                                                     <tr>
                                                         <td><?php echo $SNo ?></td>
-                                                        <td><?php echo $group->groupName ?></td>
-                                                        <td data-id="<?php echo $group->idGroup ?>">
+                                                        <td><?php echo $page->pageName ?></td>
+                                                        <td><?php echo $page->pageUrl ?></td>
+                                                        <td data-id="<?php echo $page->idPages ?>">
                                                             <a href="javascript:void(0)" onclick="getEdit(this)"><i
                                                                     class="ft-edit"></i> Edit </a>|
                                                             <a href="javascript:void(0)" onclick="getDelete(this)">
@@ -82,7 +80,8 @@
                                             <tfoot>
                                             <tr>
                                                 <th>SNo</th>
-                                                <th>Group</th>
+                                                <th>Page</th>
+                                                <th>URL</th>
                                                 <th>Action</th>
                                             </tr>
                                             </tfoot>
@@ -98,19 +97,18 @@
     </div>
 </div>
 <!-- END: Content-->
-
 <!-- Modal -->
 <div class="modal fade text-left" id="addModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel_add"
      aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header bg-primary white">
-                <h4 class="modal-title white" id="myModalLabel_add">Add Group</h4>
+                <h4 class="modal-title white" id="myModalLabel_add">Add Page</h4>
             </div>
             <div class="modal-body">
-                <div class="form-group">
-                    <label for="groupName">Group: </label>
-                    <input type="text" class="form-control groupName" id="groupName">
+                <div class="form-page">
+                    <label for="pageName">Page: </label>
+                    <input type="text" class="form-control pageName" id="pageName">
                 </div>
             </div>
             <div class="modal-footer">
@@ -121,21 +119,20 @@
         </div>
     </div>
 </div>
-
 <div class="modal fade text-left" id="editModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel_edit"
      aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header bg-primary white">
-                <h4 class="modal-title white" id="myModalLabel_edit">Edit Group</h4>
+                <h4 class="modal-title white" id="myModalLabel_edit">Edit Page</h4>
 
-                <input type="hidden" id="edit_idGroup" name="edit_idGroup">
+                <input type="hidden" id="edit_idPage" name="edit_idPage">
             </div>
             <div class="modal-body">
 
-                <div class="form-group">
-                    <label for="edit_groupName">Group: </label>
-                    <input type="text" class="form-control edit_groupName" id="edit_groupName">
+                <div class="form-page">
+                    <label for="edit_pageName">Page: </label>
+                    <input type="text" class="form-control edit_pageName" id="edit_pageName">
                 </div>
             </div>
             <div class="modal-footer">
@@ -153,8 +150,8 @@
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header bg-primary white">
-                <h4 class="modal-title white" id="myModalLabel_delete">Delete Group</h4>
-                <input type="hidden" id="delete_idGroup" name="delete_idGroup">
+                <h4 class="modal-title white" id="myModalLabel_delete">Delete Page</h4>
+                <input type="hidden" id="delete_idPage" name="delete_idPage">
             </div>
             <div class="modal-body">
                 <p>Are you sure, you want to delete this?</p>
@@ -176,7 +173,7 @@
 <script>
     $(document).ready(function () {
         $('.mysettings').addClass('open');
-        $('.group_view').addClass('active');
+        $('.page_view').addClass('active');
         $('#my_table_pro').DataTable();
         $('.addbtn').click(function () {
             $('#addModal').modal('show');
@@ -184,23 +181,23 @@
     });
 
     function addData() {
-        $('#groupName').css('border', '1px solid #babfc7');
+        $('#pageName').css('border', '1px solid #babfc7');
         var data = {};
-        data['groupName'] = $('#groupName').val();
-        if (data['groupName'] == '' || data['groupName'] == undefined) {
-            $('#groupName').css('border', '1px solid red');
-            toastMsg('Group', 'Invalid Group Name', 'error');
+        data['pageName'] = $('#pageName').val();
+        if (data['pageName'] == '' || data['pageName'] == undefined) {
+            $('#pageName').css('border', '1px solid red');
+            toastMsg('Page', 'Invalid Page Name', 'error');
         } else {
             showloader();
             $('.mybtn').attr('disabled', 'disabled');
-            CallAjax('<?php echo base_url('index.php/Settings/addGroupData'); ?>', data, 'POST', function (result) {
+            CallAjax('<?php echo base_url('index.php/Settings/addPageData'); ?>', data, 'POST', function (result) {
                 hideloader();
                 if (result == 1) {
                     toastMsg('Success', 'Successfully inserted', 'success');
                     $('#addModal').modal('hide');
                     window.location.reload();
                 } else if (result == 3) {
-                    toastMsg('Group', 'Invalid Group Name', 'error');
+                    toastMsg('Page', 'Invalid Page Name', 'error');
                 } else {
                     toastMsg('Error', 'Something went wrong', 'error');
                 }
@@ -210,30 +207,30 @@
 
     function getDelete(obj) {
         var id = $(obj).parent('td').attr('data-id');
-        $('#delete_idGroup').val(id);
+        $('#delete_idPage').val(id);
         $('#deleteModal').modal('show');
     }
 
     function deletePage() {
         var data = {};
-        data['idGroup'] = $('#delete_idGroup').val();
-        if (data['idGroup'] == '' || data['idGroup'] == undefined || data['idGroup'] == 0) {
-            toastMsg('Group', 'Something went wrong', 'error');
+        data['idPage'] = $('#delete_idPage').val();
+        if (data['idPage'] == '' || data['idPage'] == undefined || data['idPage'] == 0) {
+            toastMsg('Page', 'Something went wrong', 'error');
             return false;
         } else {
-            CallAjax('<?php echo base_url('index.php/Settings/deleteGroupData')?>', data, 'POST', function (res) {
+            CallAjax('<?php echo base_url('index.php/Settings/deletePageData')?>', data, 'POST', function (res) {
 
                 if (res == 1) {
-                    toastMsg('Group', 'Successfully Deleted', 'success');
+                    toastMsg('Page', 'Successfully Deleted', 'success');
                     setTimeout(function () {
 
                         $('#deleteModal').modal('hide');
                         window.location.reload();
                     }, 500);
                 } else if (res == 2) {
-                    toastMsg('Group', 'Something went wrong', 'error');
+                    toastMsg('Page', 'Something went wrong', 'error');
                 } else if (res == 3) {
-                    toastMsg('Group', 'Invalid Group', 'error');
+                    toastMsg('Page', 'Invalid Page', 'error');
                 }
 
             });
@@ -244,12 +241,12 @@
         var data = {};
         data['id'] = $(obj).parent('td').attr('data-id');
         if (data['id'] != '' && data['id'] != undefined) {
-            CallAjax('<?php echo base_url('index.php/Settings/getGroupEdit')?>', data, 'POST', function (result) {
+            CallAjax('<?php echo base_url('index.php/Settings/getPageEdit')?>', data, 'POST', function (result) {
                 if (result != '' && JSON.parse(result).length > 0) {
                     var a = JSON.parse(result);
                     try {
-                        $('#edit_idGroup').val(data['id']);
-                        $('#edit_groupName').val(a[0]['groupName']);
+                        $('#edit_idPage').val(data['id']);
+                        $('#edit_pageName').val(a[0]['pageName']);
                     } catch (e) {
                     }
                     $('#editModal').modal('show');
@@ -261,33 +258,33 @@
     }
 
     function editData() {
-        $('#edit_groupName').css('border', '1px solid #babfc7');
+        $('#edit_pageName').css('border', '1px solid #babfc7');
         var flag = 0;
         var data = {};
-        data['idGroup'] = $('#edit_idGroup').val();
-        data['groupName'] = $('#edit_groupName').val();
-        if (data['idGroup'] == '' || data['idGroup'] == undefined || data['idGroup'].length < 1) {
+        data['idPage'] = $('#edit_idPage').val();
+        data['pageName'] = $('#edit_pageName').val();
+        if (data['idPage'] == '' || data['idPage'] == undefined || data['idPage'].length < 1) {
             flag = 1;
             return false;
         }
-        if (data['groupName'] == '' || data['groupName'] == undefined || data['groupName'].length < 1) {
-            $('#edit_groupName').css('border', '1px solid red');
-            toastMsg('Group', 'Invalid Group Name', 'error');
+        if (data['pageName'] == '' || data['pageName'] == undefined || data['pageName'].length < 1) {
+            $('#edit_pageName').css('border', '1px solid red');
+            toastMsg('Page', 'Invalid Page Name', 'error');
             flag = 1;
             return false;
         }
         if (flag === 0) {
-            CallAjax('<?php echo base_url('index.php/Settings/editGroupData')?>', data, 'POST', function (res) {
+            CallAjax('<?php echo base_url('index.php/Settings/editPageData')?>', data, 'POST', function (res) {
                 if (res == 1) {
-                    toastMsg('Group', 'Successfully Edited', 'success');
+                    toastMsg('Page', 'Successfully Edited', 'success');
                     setTimeout(function () {
                         $('#editModal').modal('hide');
                         window.location.reload();
                     }, 500);
                 } else if (res == 2) {
-                    toastMsg('Group', 'Something went wrong', 'error');
+                    toastMsg('Page', 'Something went wrong', 'error');
                 } else if (res == 3) {
-                    toastMsg('Group', 'Invalid Group', 'error');
+                    toastMsg('Page', 'Invalid Page', 'error');
                 }
             });
         }
--- assets/uploads/myfiles/savedraft.java
+++ assets/uploads/myfiles/savedraft.java
@@ -1,37 +1,188 @@
 JSONObject f1 = new JSONObject(); 
-f1.put("a401", 
-bi.a401a.isChecked() ?"1" : 
-bi.a401b.isChecked() ?"2" : 
-bi.a401c.isChecked() ?"3" : 
-bi.a401d.isChecked() ?"4" : 
-bi.a401x.isChecked() ?"96" : 
+f1.put("f101", 
+bi.f101n.isChecked() ?"2" : 
+bi.f101y.isChecked() ?"1" : 
  "0"); 
-f1.put("a401xt", bi.a401xt.getText().toString());
-f1.put("a402", 
-bi.a402a.isChecked() ?"1" : 
-bi.a402b.isChecked() ?"2" : 
+f1.put("f101aa",bi.f101aa.isChecked() ?"1" :"0");
+f1.put("f101ab",bi.f101ab.isChecked() ?"2" :"0");
+f1.put("f101ac",bi.f101ac.isChecked() ?"3" :"0");
+f1.put("f101ad",bi.f101ad.isChecked() ?"4" :"0");
+f1.put("f101ae",bi.f101ae.isChecked() ?"5" :"0");
+f1.put("f101af",bi.f101af.isChecked() ?"6" :"0");
+f1.put("f101ag",bi.f101ag.isChecked() ?"7" :"0");
+f1.put("f101ah",bi.f101ah.isChecked() ?"8" :"0");
+f1.put("f101ai",bi.f101ai.isChecked() ?"9" :"0");
+f1.put("f102a",bi.f102a.isChecked() ?"1" :"0");
+f1.put("f102b",bi.f102b.isChecked() ?"2" :"0");
+f1.put("f102c",bi.f102c.isChecked() ?"3" :"0");
+f1.put("f102d",bi.f102d.isChecked() ?"4" :"0");
+f1.put("f103a",bi.f103a.isChecked() ?"1" :"0");
+f1.put("f103b",bi.f103b.isChecked() ?"2" :"0");
+f1.put("f103c",bi.f103c.isChecked() ?"3" :"0");
+f1.put("f103d",bi.f103d.isChecked() ?"4" :"0");
+f1.put("f103e",bi.f103e.isChecked() ?"5" :"0");
+f1.put("f104", bi.f104.getText().toString());
+f1.put("f105", 
+bi.f105a.isChecked() ?"1" : 
+bi.f105b.isChecked() ?"2" : 
+bi.f105c.isChecked() ?"3" : 
+bi.f105d.isChecked() ?"4" : 
  "0"); 
-f1.put("a403a",bi.a403a.isChecked() ?"1" :"0");
-f1.put("a403b",bi.a403b.isChecked() ?"2" :"0");
-f1.put("a403c",bi.a403c.isChecked() ?"3" :"0");
-f1.put("a403d",bi.a403d.isChecked() ?"4" :"0");
-f1.put("a403e",bi.a403e.isChecked() ?"97" :"0");
-f1.put("a404", 
-bi.a404a.isChecked() ?"1" : 
-bi.a404b.isChecked() ?"2" : 
+f1.put("f106", bi.f106.getText().toString());
+f1.put("f107", 
+bi.f107a.isChecked() ?"1" : 
+bi.f107b.isChecked() ?"6" : 
+bi.f107x.isChecked() ?"96" : 
  "0"); 
-f1.put("a405a",bi.a405a.isChecked() ?"1" :"0");
-f1.put("a405b",bi.a405b.isChecked() ?"2" :"0");
-f1.put("a405c",bi.a405c.isChecked() ?"3" :"0");
-f1.put("a405d",bi.a405d.isChecked() ?"4" :"0");
-f1.put("a405e",bi.a405e.isChecked() ?"97" :"0");
-f1.put("a406a_title",bi.a406a_title.isChecked() ?"" :"0");
-f1.put("a406a",bi.a406a.isChecked() ?"1" :"0");
-f1.put("a406b",bi.a406b.isChecked() ?"2" :"0");
-f1.put("a406c",bi.a406c.isChecked() ?"3" :"0");
-f1.put("a406d",bi.a406d.isChecked() ?"4" :"0");
-f1.put("a406e",bi.a406e.isChecked() ?"5" :"0");
-f1.put("a406f",bi.a406f.isChecked() ?"6" :"0");
-f1.put("a406g",bi.a406g.isChecked() ?"7" :"0");
-f1.put("a406h",bi.a406h.isChecked() ?"8" :"0");
-f1.put("a406i",bi.a406i.isChecked() ?"97" :"0");
+f1.put("f107bt", bi.f107bt.getText().toString());
+f1.put("f107xt", bi.f107xt.getText().toString());
+f1.put("f108", 
+bi.f108a.isChecked() ?"1" : 
+bi.f108b.isChecked() ?"2" : 
+bi.f108c.isChecked() ?"98" : 
+ "0"); 
+f1.put("f108at", bi.f108at.getText().toString());
+f1.put("f108bt", bi.f108bt.getText().toString());
+f1.put("f109", 
+bi.f109a.isChecked() ?"1" : 
+bi.f109b.isChecked() ?"98" : 
+ "0"); 
+f1.put("f109at", bi.f109at.getText().toString());
+f1.put("f110a",bi.f110a.isChecked() ?"1" :"0");
+f1.put("f110b",bi.f110b.isChecked() ?"2" :"0");
+f1.put("f110c",bi.f110c.isChecked() ?"3" :"0");
+f1.put("f110d",bi.f110d.isChecked() ?"4" :"0");
+f1.put("f110e",bi.f110e.isChecked() ?"5" :"0");
+f1.put("f110f",bi.f110f.isChecked() ?"6" :"0");
+f1.put("f110g",bi.f110g.isChecked() ?"7" :"0");
+f1.put("f110h",bi.f110h.isChecked() ?"8" :"0");
+f1.put("f110i",bi.f110i.isChecked() ?"9" :"0");
+f1.put("f110x",bi.f110x.isChecked() ?"96" :"0");
+f1.put("f110xt", bi.f110xt.getText().toString());
+f1.put("f111", 
+bi.f111a.isChecked() ?"1" : 
+bi.f111b.isChecked() ?"2" : 
+ "0"); 
+f1.put("f112", 
+bi.f112a.isChecked() ?"1" : 
+bi.f112b.isChecked() ?"2" : 
+bi.f112c.isChecked() ?"98" : 
+ "0"); 
+f1.put("f113", 
+bi.f113a.isChecked() ?"1" : 
+bi.f113b.isChecked() ?"98" : 
+ "0"); 
+f1.put("f113at", bi.f113at.getText().toString());
+f1.put("f114", 
+bi.f114a.isChecked() ?"1" : 
+bi.f114b.isChecked() ?"2" : 
+ "0"); 
+f1.put("f115a",bi.f115a.isChecked() ?"1" :"0");
+f1.put("f115b",bi.f115b.isChecked() ?"2" :"0");
+f1.put("f115c",bi.f115c.isChecked() ?"3" :"0");
+f1.put("f115d",bi.f115d.isChecked() ?"4" :"0");
+f1.put("f115e",bi.f115e.isChecked() ?"5" :"0");
+f1.put("f115f",bi.f115f.isChecked() ?"6" :"0");
+f1.put("f116a",bi.f116a.isChecked() ?"1" :"0");
+f1.put("f116b",bi.f116b.isChecked() ?"2" :"0");
+f1.put("f116c",bi.f116c.isChecked() ?"3" :"0");
+f1.put("f116d",bi.f116d.isChecked() ?"4" :"0");
+f1.put("f116e",bi.f116e.isChecked() ?"5" :"0");
+f1.put("f116f",bi.f116f.isChecked() ?"6" :"0");
+f1.put("f116g",bi.f116g.isChecked() ?"7" :"0");
+f1.put("f116h",bi.f116h.isChecked() ?"8" :"0");
+f1.put("f116i",bi.f116i.isChecked() ?"9" :"0");
+f1.put("f117", 
+bi.f117a.isChecked() ?"1" : 
+bi.f117b.isChecked() ?"2" : 
+bi.f117c.isChecked() ?"3" : 
+bi.f117d.isChecked() ?"4" : 
+bi.f117e.isChecked() ?"5" : 
+ "0"); 
+f1.put("f118", 
+bi.f118a.isChecked() ?"1" : 
+bi.f118b.isChecked() ?"2" : 
+ "0"); 
+f1.put("f118at", bi.f118at.getText().toString());
+f1.put("f118bt", bi.f118bt.getText().toString());
+f1.put("f119", 
+bi.f119a.isChecked() ?"1" : 
+bi.f119b.isChecked() ?"2" : 
+ "0"); 
+f1.put("f120", bi.f120.getText().toString());
+f1.put("f121", 
+bi.f121a.isChecked() ?"1" : 
+bi.f121b.isChecked() ?"2" : 
+ "0"); 
+f1.put("f122", bi.f122.getText().toString());
+f1.put("f123a",bi.f123a.isChecked() ?"1" :"0");
+f1.put("f123b",bi.f123b.isChecked() ?"2" :"0");
+f1.put("f123c",bi.f123c.isChecked() ?"3" :"0");
+f1.put("f123d",bi.f123d.isChecked() ?"4" :"0");
+f1.put("f123e",bi.f123e.isChecked() ?"5" :"0");
+f1.put("f123f",bi.f123f.isChecked() ?"6" :"0");
+f1.put("f123g",bi.f123g.isChecked() ?"7" :"0");
+f1.put("f124", 
+bi.f124a.isChecked() ?"1" : 
+bi.f124b.isChecked() ?"2" : 
+ "0"); 
+f1.put("f125a",bi.f125a.isChecked() ?"1" :"0");
+f1.put("f125b",bi.f125b.isChecked() ?"2" :"0");
+f1.put("f125c",bi.f125c.isChecked() ?"3" :"0");
+f1.put("f125d",bi.f125d.isChecked() ?"4" :"0");
+f1.put("f125e",bi.f125e.isChecked() ?"5" :"0");
+f1.put("f125f",bi.f125f.isChecked() ?"6" :"0");
+f1.put("f126a",bi.f126a.isChecked() ?"1" :"0");
+f1.put("f126b",bi.f126b.isChecked() ?"2" :"0");
+f1.put("f126c",bi.f126c.isChecked() ?"3" :"0");
+f1.put("f126d",bi.f126d.isChecked() ?"4" :"0");
+f1.put("f126e",bi.f126e.isChecked() ?"5" :"0");
+f1.put("f126f",bi.f126f.isChecked() ?"6" :"0");
+f1.put("f126g",bi.f126g.isChecked() ?"7" :"0");
+f1.put("f127", 
+bi.f127a.isChecked() ?"1" : 
+bi.f127b.isChecked() ?"2" : 
+bi.f127c.isChecked() ?"3" : 
+ "0"); 
+f1.put("f128a",bi.f128a.isChecked() ?"1" :"0");
+f1.put("f128b",bi.f128b.isChecked() ?"2" :"0");
+f1.put("f128c",bi.f128c.isChecked() ?"3" :"0");
+f1.put("f128d",bi.f128d.isChecked() ?"4" :"0");
+f1.put("f128e",bi.f128e.isChecked() ?"5" :"0");
+f1.put("f128f",bi.f128f.isChecked() ?"6" :"0");
+f1.put("f128g",bi.f128g.isChecked() ?"7" :"0");
+f1.put("f129", 
+bi.f129a.isChecked() ?"1" : 
+bi.f129b.isChecked() ?"2" : 
+ "0"); 
+f1.put("f130a",bi.f130a.isChecked() ?"1" :"0");
+f1.put("f130b",bi.f130b.isChecked() ?"2" :"0");
+f1.put("f130c",bi.f130c.isChecked() ?"3" :"0");
+f1.put("f130d",bi.f130d.isChecked() ?"4" :"0");
+f1.put("f130e",bi.f130e.isChecked() ?"5" :"0");
+f1.put("f130f",bi.f130f.isChecked() ?"6" :"0");
+f1.put("f130g",bi.f130g.isChecked() ?"7" :"0");
+f1.put("f130h",bi.f130h.isChecked() ?"8" :"0");
+f1.put("f130i",bi.f130i.isChecked() ?"9" :"0");
+f1.put("f131", 
+bi.f131a.isChecked() ?"1" : 
+bi.f131b.isChecked() ?"2" : 
+bi.f131c.isChecked() ?"3" : 
+ "0"); 
+f1.put("f132", 
+bi.f132a.isChecked() ?"1" : 
+bi.f132b.isChecked() ?"2" : 
+bi.f132c.isChecked() ?"3" : 
+ "0"); 
+f1.put("f133", 
+bi.f133a.isChecked() ?"1" : 
+bi.f133b.isChecked() ?"2" : 
+bi.f133c.isChecked() ?"3" : 
+ "0"); 
+f1.put("f134a",bi.f134a.isChecked() ?"1" :"0");
+f1.put("f134b",bi.f134b.isChecked() ?"2" :"0");
+f1.put("f134c",bi.f134c.isChecked() ?"3" :"0");
+f1.put("f134d",bi.f134d.isChecked() ?"4" :"0");
+f1.put("f134e",bi.f134e.isChecked() ?"5" :"0");
+f1.put("f134f",bi.f134f.isChecked() ?"6" :"0");
+f1.put("f134g",bi.f134g.isChecked() ?"7" :"0");
