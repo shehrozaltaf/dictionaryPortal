ff47184f1fc2524a415e095f172678e7a29bf1c2
shahroz.khan <shehroz.altaf92@gmail.com>
2020-11-26 17:17:42 +0500

    title

--- assets/uploads/myfiles/savedraft.java
+++ assets/uploads/myfiles/savedraft.java
@@ -1,8 +1,63 @@
 JSONObject json = new JSONObject(); 
-json.put("wf101", bi.wf10101.isChecked() ? "1" 
- : bi.wf10102.isChecked() ? "2" 
+json.put("db01", bi.db01a.isChecked() ? "1" 
+ : bi.db01b.isChecked() ? "2" 
  :  "-1"); 
 
-json.put("wf10102x", bi.wf10102x.getText().toString());
-json.put("wfi02", bi.wfi02.getText().toString());
+json.put("db02", bi.db02a.isChecked() ? "1" 
+ : bi.db02b.isChecked() ? "2" 
+ :  "-1"); 
 
+json.put("db03", bi.db03a.isChecked() ? "1" 
+ : bi.db03b.isChecked() ? "2" 
+ :  "-1"); 
+
+json.put("db04", bi.db04a.isChecked() ? "1" 
+ : bi.db04b.isChecked() ? "2" 
+ :  "-1"); 
+
+json.put("db05", bi.db05a.isChecked() ? "1" 
+ : bi.db05b.isChecked() ? "2" 
+ :  "-1"); 
+
+json.put("db06", bi.db06a.isChecked() ? "1" 
+ : bi.db06b.isChecked() ? "2" 
+ :  "-1"); 
+
+json.put("db07", bi.db07a.isChecked() ? "1" 
+ : bi.db07b.isChecked() ? "2" 
+ :  "-1"); 
+
+json.put("db08", bi.db08a.isChecked() ? "1" 
+ : bi.db08b.isChecked() ? "2" 
+ :  "-1"); 
+
+json.put("db09", bi.db09a.isChecked() ? "1" 
+ : bi.db09b.isChecked() ? "2" 
+ :  "-1"); 
+
+json.put("db10", bi.db10a.isChecked() ? "1" 
+ : bi.db10b.isChecked() ? "2" 
+ :  "-1"); 
+
+json.put("db11", bi.db11a.isChecked() ? "1" 
+ : bi.db11b.isChecked() ? "2" 
+ :  "-1"); 
+
+json.put("db12", bi.db12a.isChecked() ? "1" 
+ : bi.db12b.isChecked() ? "2" 
+ : bi.db12c.isChecked() ? "3" 
+ :  "-1"); 
+
+json.put("db13", bi.db13a.isChecked() ? "1" 
+ : bi.db13b.isChecked() ? "2" 
+ :  "-1"); 
+
+json.put("db14", bi.db14.getText().toString());
+
+json.put("db15", bi.db15.getText().toString());
+
+json.put("db16", bi.db16a.isChecked() ? "1" 
+ : bi.db1696.isChecked() ? "96" 
+ :  "-1"); 
+
+json.put("db1696x", bi.db1696x.getText().toString());
